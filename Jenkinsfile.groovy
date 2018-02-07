0ecf1c43b01f58f35bca73940f9bb114458978a8
isanmartin <isanmartin@keedio.com>
2018-02-07 16:32:02 +0100

    Modify Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,28 +2,32 @@
 @Library('msa-cicd-jenkins-shared-libs')_
 import com.evobanco.Utils
 
-def utils = new com.evobanco.Utils()
 
-def artifactorySnapshotsURL = 'https://digitalservices.evobanco.com/artifactory/libs-snapshot-local'
-def artifactoryReleasesURL = 'https://digitalservices.evobanco.com/artifactory/libs-release-local'
-def sonarQube = 'http://sonarqube:9000'
-def openshiftURL = 'https://openshift.grupoevo.corp:8443'
-def openshiftCredential = 'openshift'
-def registry = '172.20.253.34'
-def artifactoryCredential = 'artifactory-token'
-def jenkinsNamespace = 'cicd'
-def mavenCmd = 'mvn -U -B -s /opt/evo-maven-settings/evo-maven-settings.xml'
-def mavenProfile = ''
-def springProfile = ''
-def params
-def envLabel
-def branchName
-def branchNameHY
-def branchType
-def artifactoryRepoURL
 
 
 def runJenkinsfile() {
+
+    def utils = new com.evobanco.Utils()
+
+    def artifactorySnapshotsURL = 'https://digitalservices.evobanco.com/artifactory/libs-snapshot-local'
+    def artifactoryReleasesURL = 'https://digitalservices.evobanco.com/artifactory/libs-release-local'
+    def sonarQube = 'http://sonarqube:9000'
+    def openshiftURL = 'https://openshift.grupoevo.corp:8443'
+    def openshiftCredential = 'openshift'
+    def registry = '172.20.253.34'
+    def artifactoryCredential = 'artifactory-token'
+    def jenkinsNamespace = 'cicd'
+    def mavenCmd = 'mvn -U -B -s /opt/evo-maven-settings/evo-maven-settings.xml'
+    def mavenProfile = ''
+    def springProfile = ''
+    def params
+    def envLabel
+    def branchName
+    def branchNameHY
+    def branchType
+    def artifactoryRepoURL
+
+
     node('maven') {
         echo "BEGIN...(PGC)"
 
